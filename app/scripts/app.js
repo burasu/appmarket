647b534d94b26f3a14b65198476f0f25010f2e4a
Blas Fernández Segura <burasu@gmail.com>
2015-09-18 11:42:50 +0200

    Definición del controlador principal.

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('AppMarketApp', ['AppMarketApp.controllers']);
+
+})();
---
+++ app/scripts/controllers.js
@@ -0,0 +1,16 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('AppMarketApp.controllers', []);
+
+    function MainController($scope)
+    {
+
+    }
+
+    angular
+        .module('AppMarketApp')
+        .controller('MainController', MainController);
+
+})();
